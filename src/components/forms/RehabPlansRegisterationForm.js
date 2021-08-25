556c1f19e9d81f02e0e966a727a35d6a6c03bbe5
Ali Raza <raarain99@gmail.com>
2021-08-25 22:04:30 +0500

    Added Rehab Plan Registeration Form Component

---
+++ src/components/forms/RehabPlansRegisterationForm.js
@@ -0,0 +1,69 @@
+import React, {useState} from 'react'
+
+
+const RehabPlansRegisterationForm = () => {
+    
+    const [userRegisteration, setuserRegisteration] = useState({
+        title: "",
+        duration: "",
+        type: "",
+        fee: "",
+        description:"",     
+    });
+
+    const handleInput = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setuserRegisteration({...userRegisteration, [name] : value})
+    }
+
+    const handleSubmit = (e) => {
+        //send to backend from here
+        e.preventDefault();
+        
+        const newuser = {...userRegisteration, id: new Date().getTime().toString() }
+        console.log(newuser)
+        setuserRecord([...userRecord, newuser]);
+        console.log(newuser)
+
+        setuserRegisteration({title: "",
+        duration: "",
+        type: "",
+        fee: "",
+        description:"",  
+        });
+    }
+
+    const [userRecord, setuserRecord] = useState([])
+    
+    
+    return (
+        <React.Fragment>
+            <form action="" onSubmit={handleSubmit}>
+                <table>
+                    <tr>
+                        <td align="right"><label htmlFor="title">Title </label></td><td><input type="string" required autoComplete="off" value={userRegisteration.title} onChange={handleInput} name="title" id="title"/></td>
+                    </tr>
+                    <tr>
+                        <td align="right"><label htmlFor="duration">Duration </label></td><td><input type="number" required autoComplete="off" value={userRegisteration.duration} onChange={handleInput} name="duration" id="duration"/></td>
+                    </tr>
+                    <tr>
+                        <td align="right"><label htmlFor="type">Type </label></td><td><input type="string" required autoComplete="off" value={userRegisteration.type} onChange={handleInput} name="type" id="type"/></td>
+                    </tr>
+                    <tr>
+                        <td align="right"><label htmlFor="fee">Fee </label></td><td><input type="number" required autoComplete="off" value={userRegisteration.fee} onChange={handleInput} name="fee" id="fee"/></td>
+                    </tr>
+                    <tr>
+                        <td align="right"><label htmlFor="description">Description </label></td><td><textarea rows="5" cols="50"  required autoComplete="off" value={userRegisteration.description} onChange={handleInput} name="description" id="description"/></td>
+                    </tr>
+                    <tr>
+                        <td align="right"></td><td><button type="submit">Submit</button></td>
+                    </tr>
+                </table>
+            </form>
+        </React.Fragment>
+    )
+}
+
+export default RehabPlansRegisterationForm
