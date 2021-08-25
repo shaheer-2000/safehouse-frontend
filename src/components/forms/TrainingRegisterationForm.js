3468d65bfab45030633de2e37284811be93e8512
Ali Raza <raarain99@gmail.com>
2021-08-25 21:36:40 +0500

    Added Training Registeration Form Component

--- src/components/forms/TrainingRegisterationForm.js
+++ src/components/forms/TrainingRegisterationForm.js
@@ -1,14 +1,15 @@
 import React, {useState} from 'react'
 
 
-const JobRegisterationForm = () => {
+const TrainingRegisterationForm = () => {
     
     const [userRegisteration, setuserRegisteration] = useState({
         discipline: "",
         title: "",
-        designation: "",
-        salary: "",
-        experience: "",
+        duration: "",
+        required_experience_level: "",
+        certification: "",
+        fee: "",
         description:"",     
     });
 
@@ -30,9 +31,10 @@ const JobRegisterationForm = () => {
 
         setuserRegisteration({discipline: "",
         title: "",
-        designation: "",
-        salary: "",
-        experience: "",
+        duration: "",
+        required_experience_level: "",
+        certification: "",
+        fee: "",
         description:"",  
         });
     }
@@ -51,13 +53,17 @@ const JobRegisterationForm = () => {
                         <td align="right"><label htmlFor="title">Title </label></td><td><input type="string" required autoComplete="off" value={userRegisteration.title} onChange={handleInput} name="title" id="title"/></td>
                     </tr>
                     <tr>
-                        <td align="right"><label htmlFor="designation">Designation </label></td><td><input type="string" required autoComplete="off" value={userRegisteration.designation} onChange={handleInput} name="designation" id="designation"/></td>
+                        <td align="right"><label htmlFor="duration">Duration </label></td><td><input type="number" required autoComplete="off" value={userRegisteration.duration} onChange={handleInput} name="duration" id="duration"/></td>
                     </tr>
                     <tr>
-                        <td align="right"><label htmlFor="salary">Salary </label></td><td><input type="number" required autoComplete="off" value={userRegisteration.salary} onChange={handleInput} name="salary" id="salary"/></td>
+                        <td align="right"><label htmlFor="required_experience_level">Required experience level </label></td><td><input type="number" required autoComplete="off" value={userRegisteration.required_experience_level} onChange={handleInput} name="required_experience_level" id="required_experience_level"/></td>
                     </tr>
                     <tr>
-                        <td align="right"><label htmlFor="experience">Experience </label></td><td><input type="number" required autoComplete="off" value={userRegisteration.experience} onChange={handleInput} name="experience" id="experience"/></td>
+                        <td align="right"><label htmlFor="certification">Certification </label></td><td><input type="radio" autoComplete="off" value="available" onChange={handleInput} name="certification" id="available"/><label>Available</label>
+                    <input type="radio" autoComplete="off" value="unavailable" onChange={handleInput} name="certification" id="unavailable"/><label>Unavailable</label></td>
+                    </tr>
+                    <tr>
+                        <td align="right"><label htmlFor="fee">Fee </label></td><td><input type="number" required autoComplete="off" value={userRegisteration.fee} onChange={handleInput} name="fee" id="fee"/></td>
                     </tr>
                     <tr>
                         <td align="right"><label htmlFor="description">Description </label></td><td><textarea rows="5" cols="50"  required autoComplete="off" value={userRegisteration.description} onChange={handleInput} name="description" id="description"/></td>
@@ -71,4 +77,4 @@ const JobRegisterationForm = () => {
     )
 }
 
-export default JobRegisterationForm
+export default TrainingRegisterationForm
